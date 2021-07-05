4adefa0e98a6dd4b2d7e7a64f7638edd0e8c09a2
tlsdbalby <1779964774@qq.com>
2021-07-05 16:18:03 +0800

    修改14

--- 14最长前缀.py
+++ 14最长前缀.py
@@ -1,17 +1,16 @@
 def longestCommonPrefix(strs: list) -> str:
     if (len(strs) == 1):
-        print("here!")
         return strs[0]
     p1 = 0
     sl = len(strs[0])
     if (sl == 0):
-        return "-2"
+        return ""
     while p1 < sl:
         char = strs[0][p1]
         for p2 in range(1, len(strs)):
             l = len(strs[p2])
             if (l == 0):
-                return "-1"
+                return ""
             if (p1 < l):
                 if char == strs[p2][p1]:
                     continue
@@ -22,4 +21,3 @@ def longestCommonPrefix(strs: list) -> str:
 
 
 print(longestCommonPrefix(["flower", "flower"]))
-print("flw"[:5])
